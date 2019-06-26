2bea660eb7f816a32bcdb25af2197bda8d5dd740
gwn <dev@gwn.wtf>
2019-06-26 11:10:24 +0300

    hey

---
+++ script/main.js
@@ -0,0 +1,14 @@
+$(function() {
+  $.backstretch("/img/bg.jpg", {
+    // centeredX: false,
+    // centeredY: false,
+    // duration: "normal"
+  });
+
+  // Translucent backround objects
+  $(".trans-bg").each(function() {
+    $("<div></div>")
+      .addClass("bg-helper")
+      .appendTo($(this));
+  });
+});
